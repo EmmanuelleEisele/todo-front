1ad78acdff8f76193c27471f2a148f7bdca5cb02
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2025-12-09 16:36:40 +0100

    fix route auth/me non existing to auth/

--- src/types/authApi.ts
+++ src/types/authApi.ts
@@ -3,7 +3,7 @@ import type { User } from "./todoApi";
 
 export async function checkTokenValid(): Promise<User | null> {
   try {
-    const res = await apiClient.get("/auth/me");
+    const res = await apiClient.get("/auth/");
     return res.data.user as User;
   } catch {
     return null;
