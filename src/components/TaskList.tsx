a47b937d0d2da54a12f4b9aef2fe84e98dcd8e15
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2026-01-20 15:34:22 +0100

    adding delete task option on all tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -93,7 +93,7 @@ export default function TaskList({
                 <p className="sm:mb-0 break-words whitespace-normal max-w-[calc(100%-3rem)] sm:max-w-md w-full">
                   {task.title}
                 </p>
-                <div>
+                <div className="flex flex-col">
                 {task.isArchived ? (
                   <button
                     onClick={() => openConfirmation(task._id)}
